a736ea760621a1c41d9e656a91e68812a430f61c
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-11-05 09:08:51 +1100

    Fix some bootstrap 5 upgrade issues

--- src/MVC5/MvcMusicStore/App_Start/BundleConfig.cs
+++ src/MVC5/MvcMusicStore/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace MvcMusicStore
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.bundle.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
